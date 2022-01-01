9d8a05e574c6a5d641e88c962c5fa567653ad2d0
Ben <spartanhooah@gmail.com>
2022-01-01 13:10:10 -0500

    still working on day 3 pt 2

--- src/main/groovy/Day3Solution.groovy
+++ src/main/groovy/Day3Solution.groovy
@@ -1,78 +1,70 @@
 import static java.lang.Integer.parseInt
 
 def report = []
+def zerosCounts = []
+def onesCounts = []
+def firstLine = true
 
 new File("TestInput.txt").eachLine {
     if (it.length() > 0) {
         report << it
+
+        if (firstLine) {
+            it.each { digit ->
+                zerosCounts << 0
+                onesCounts << 0
+            }
+
+            firstLine = false
+        }
+
+        for (int i : 0..it.length() - 1) {
+            it[i] == "0" ? zerosCounts[i]++ : onesCounts[i]++
+        }
     }
 }
 
 // Part 1
 def gamma = ""
 def epsilon = ""
-def zerosCount = 0
-def onesCount = 0
 
 for (i in 0..report[0].length() - 1) {
-    report.each {
-        switch(it[i]) {
-            case "0":
-                zerosCount++
-                break
-            case "1":
-                onesCount++
-                break
-            default:
-                break
-        }
-    }
-
-    gamma += (zerosCount > onesCount) ? "0" : "1"
-    epsilon += (zerosCount > onesCount) ? "1" : "0"
-
-    zerosCount = 0
-    onesCount = 0
+    gamma += (zerosCounts[i] > onesCounts[i]) ? "0" : "1"
+    epsilon += (zerosCounts[i] > onesCounts[i]) ? "1" : "0"
 }
 
 println parseInt(gamma, 2) * parseInt(epsilon, 2)
 
 // Part 2
-def oxygenList = []
+def oxygenList = report.clone()
 def oxygenRating = 0
-def co2List = []
+def co2List = report.clone()
 def co2Rating = 0
 
 for (i in 0..report[0].length() - 1) {
     report.each {
-        switch (it[i]) {
-            case "0":
-                zerosCount++
-                break
-            case "1":
-                onesCount++
-                break
-            default:
-                break
-        }
-    }
-
-    report.each {
-        if (zerosCount > onesCount) {
+        if (onesCounts[i] >= zerosCounts[i]) {
             if (it[i] == "0") {
-                oxygenList << it
+                oxygenList.remove(it)
             } else {
-                co2List << it
+                co2List.remove(it)
             }
         } else {
             if (it[i] == "1") {
-                oxygenList << it
+                oxygenList.remove(it)
             } else {
-                co2List << it
+                co2List.remove(it)
             }
         }
-    }
 
-    zerosCount = 0
-    onesCount = 0
+        if (oxygenList.size() == 1) {
+            oxygenRating = parseInt(oxygenList[0], 2)
+        }
+
+        if (co2List.size() == 1) {
+            co2Rating = parseInt(co2List[0], 2)
+        }
+    }
 }
+
+println oxygenRating * co2Rating
