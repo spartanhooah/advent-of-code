7de2a87f2ad583b077a3cb8098dab4729f7db598
Ben Frey <spartanhooah@gmail.com>
2022-01-02 13:53:25 -0500

    day 4 part 1 done

---
+++ src/main/groovy/Day4Solution.groovy
@@ -0,0 +1,113 @@
+import static java.lang.Integer.parseInt
+
+def lineNumber = 0
+def drawnNumbers
+def boards = []
+def currentBoard = new Board()
+
+new File("Day4Input.txt").eachLine {
+    if (it.length() > 0) {
+        if (lineNumber == 0) {
+            drawnNumbers = it.trim().split(",").collect { parseInt(it) }
+        } else {
+            currentBoard.addRow(it.trim().split("\\s+"))
+
+            if (lineNumber % 5 == 0) {
+                boards << currentBoard
+                currentBoard = new Board()
+            }
+        }
+
+        lineNumber++
+    }
+}
+
+def anyWon = false
+
+for (int number : drawnNumbers) {
+    if (anyWon) {
+        break
+    }
+
+    boards*.markNumber(number)
+
+    for (Board board : boards) {
+        if (board.hasWon()) {
+            anyWon = true
+            println board.total() * number
+
+            break
+        }
+    }
+}
+
+class Board {
+    private def rows = []
+
+    def addRow(row) {
+        def newRow = []
+
+        row.each {
+            newRow << new Space(number: parseInt(it), marked: false)
+        }
+
+        rows << newRow
+    }
+
+    def markNumber(number) {
+        rows.each { row ->
+            row.each { space ->
+                if (space.number == number) {
+                    space.marked = true
+                }
+            }
+        }
+    }
+
+    boolean hasWon() {
+        def columns = fillColumns()
+
+        def winningRow = rows.any { row ->
+            row.every { space ->
+                space.marked
+            }
+        }
+
+        def winningColumn = columns.any { column ->
+            column.every { space ->
+                space.marked
+            }
+        }
+
+        return winningRow || winningColumn
+    }
+
+    def fillColumns() {
+        def columns = []
+
+        for (int i : 0..rows.size() - 1) {
+            columns << []
+
+            for (int j : 0..rows[0].size() - 1) {
+                columns[i][j] = rows[j][i]
+            }
+        }
+
+        return columns
+    }
+
+    int total() {
+        def total = 0
+
+        rows.each { row ->
+            total += row.findAll { !it.marked }.inject(0) { acc, val -> acc + val.number }
+        }
+
+        return total
+    }
+
+    private class Space {
+        int number
+        boolean marked
+    }
+}
